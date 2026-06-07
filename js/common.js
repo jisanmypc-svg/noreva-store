d4d567b414d0bdd4439a24ab06e43999ed41c768
jisanmypc-svg <jisan.mypc@gmail.com>
2026-06-08 01:03:44 +0600

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,22 +1,27 @@
-function updateCartCount(){
+function updateCartCount() {
 
-let cart =
-JSON.parse(
-localStorage.getItem("cart")
-) || [];
+    let cart =
+    JSON.parse(
+        localStorage.getItem("cart")
+    ) || [];
 
-const counter =
-document.getElementById(
-"cartCount"
-);
+    const counter =
+    document.getElementById("cartCount");
 
-if(counter){
-
-counter.innerText =
-cart.length;
-
-}
+    if (counter) {
+        counter.innerText = cart.length;
+    }
 
 }
 
 updateCartCount();
+
+window.addEventListener(
+    "pageshow",
+    updateCartCount
+);
+
+window.addEventListener(
+    "focus",
+    updateCartCount
+);
